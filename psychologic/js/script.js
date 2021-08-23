d079a25a73d8a06e0c17a088e2ed63cb589b7b2b
eduardkarpow <67784926+eduardkarpow@users.noreply.github.com>
2021-08-23 15:08:01 +0300

    Add files via upload

---
+++ psychologic/js/script.js
@@ -0,0 +1,53 @@
+
+const burger = document.getElementsByClassName('nav__burger')[0];
+const header = document.getElementsByClassName('head')[0];
+const nav = document.getElementsByTagName('nav')[0];
+let borders = document.getElementsByClassName('border');
+if (!borders.length) {
+    borders = document.getElementsByClassName('contact__border');
+}
+if(burger){
+    burger.onclick = () => {
+        header.classList.toggle('active');
+        nav.classList.toggle('active');
+        
+        };
+}
+
+document.onclick = event => {
+    if(event.target.classList[0] === 'feedback__input' || event.target.classList[0] === 'feedback__textarea' || event.target.classList[0] === 'contact__input' || event.target.classList[0] === 'contact__textarea') {
+        Object.values(borders).map(border => border.classList.remove('visible'));
+        event.target.parentElement.classList.add('visible');
+    }
+    else {
+        Object.values(borders).map(border => border.classList.remove('visible'));
+    }
+}
+// booking
+const select = document.getElementsByClassName('booking__select')[0];
+let options = document.getElementsByClassName('booking__option');
+for(option of options){
+    option.onclick = event => {
+        if(!select.classList[1]){
+            select.classList.add('active');
+            Object.values(options).map(option => option.classList.remove('hiden'));  
+        } else {
+            let target = event.target;
+            if(event.target.classList[0] !== 'booking__option'){
+                target = event.target.parentElement;
+            }
+        
+            Object.values(options).map(option => option.classList.add('hiden'));
+            target.classList.remove('hiden');
+            select.classList.remove('active');
+        }
+        
+    };
+}
+    
+
+/*select.onclick = () => {
+    debugger;
+    
+    
+}*/
